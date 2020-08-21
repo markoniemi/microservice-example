b74245a511b81455bc8d2c3dc42e6864f5d4b44a
Marko Niemi <marko.j.niemi@gmail.com>
2020-08-21 13:26:32 +0300

    docker-compose support. Add postgres container. Add missing dockerfiles. user-application not yet working.

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 docker run \
 -it --rm \
--p 8761:8761 \
--p 8081:8081 \
--p 8082:8082 \
+-v /var/run/docker.sock:/var/run/docker.sock \
 -v "$PWD":/usr/src \
 -v "$HOME/.m2":/root/.m2 \
 -w /usr/src \
 maven:3.6-jdk-8-alpine \
-mvn package install verify
+mvn package -DskipTests=true -P docker "$@"
+#mvn package install verify 
+#-p 8761:8761 \
+#-p 8081:8081 \
+#-p 8082:8082 \
---
+++ postgres.sh
@@ -0,0 +1,1 @@
+docker run --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres --port 5432:5432
